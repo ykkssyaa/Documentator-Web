4f6a5d99a6a276ca7b51d9c3c4864b334ac9cdea
ykkssyaa <mr.ender.03@mail.ru>
2024-03-24 16:08:08 +0300

    404, 403 errors handlers

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ from users.forms import LoginUserForm
 
 
 def page_not_found(request, exception):
-    return HttpResponseNotFound("<h1>Page not found 404</h1> <a href='/'>На главную страницу</a>")
+    return render(request, 'users/404.html', status=404)
 
 
 class LoginUser(LoginView):
@@ -30,3 +30,17 @@ def logout_user(request):
 class Settings(TemplateView):
     template_name = 'users/settings.html'
     extra_context = {'title': 'Настройки'}
+
+
+class ForbiddenMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        from django.core.exceptions import PermissionDenied
+        if isinstance(exception, PermissionDenied):
+            return render(request, 'users/403.html', status=403)
